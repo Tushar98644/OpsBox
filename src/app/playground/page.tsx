e6e2b35f0ee35f61cd6bebb2f094cd10df476157
Tushar Banik <b.toshar@iitg.ac.in>
2024-09-01 11:41:01 +0530

    file-tree setup

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,22 +1,66 @@
 'use client'
 import dynamic from 'next/dynamic';
-
-import Layout from '@/components/Layout/layout';
 import { ProjectEditor } from '@/components/editor/editor';
+// import { Explorer } from '@/components/editor/components/explorer';
+import { FileTree } from '@/components/editor/components/file-tree';
+import { Directory, File, Type } from '@/utils/file-manager';
 
 const PseudoTerminal = dynamic(
     async () => (await import('@/components/terminal')).PseudoTerminal, { ssr: false }
 );
 
+let rootDir: Directory = {
+    id: "root",
+    name: "root",
+    parentId: undefined,
+    type: Type.DIRECTORY,
+    path: "",
+    depth: 0,
+    dirs: [
+        {
+            id: "src",
+            name: "src",
+            parentId: "root",
+            type: Type.DIRECTORY,
+            files: [],
+            dirs: [],
+            path: '',
+            depth: 0
+        }
+    ],
+    files: [
+        {
+            id: "src/index.js",
+            name: "index.js",
+            parentId: "src",
+            type: Type.FILE,
+            depth: 0,
+            path: ''
+        }
+    ]
+  };
+  
 const Playground = () => {
     return (
         <div className='flex flex-row w-screen h-screen'>
-            <div className='file-tree'>file-tree</div>
-            <div className='terminal-editor flex flex-col flex-1'>
-                <div className='editor flex-2/3 h-2/3'>
+            {/* File tree taking 1/3 of the width */}
+            <div className='file-tree w-1/5 h-full'>
+                <FileTree 
+                    rootDir={rootDir} 
+                    selectedFile={undefined} 
+                    onSelect={function (file: File): void {
+                        console.log('Function not implemented.');
+                    }} 
+                />
+            </div>
+            {/* Editor and Terminal taking 2/3 of the width */}
+            <div className='terminal-editor flex flex-col w-full h-full'>
+                {/* Editor taking 2/3 of the height */}
+                <div className='editor h-2/3'>
                     <ProjectEditor />
                 </div>
-                <div className='terminal flex-1/3 h-1/3'>
+                {/* Terminal taking 1/3 of the height */}
+                <div className='terminal h-1/3'>
                     <PseudoTerminal />
                 </div>
             </div>
